6e0c0c920905538458d8d349da5b3dd1d872fdfa
Daniel S. Standage <daniel.standage@gmail.com>
2017-05-17 16:34:24 -0400

    Cleanup

--- README.md
+++ README.md
@@ -7,6 +7,12 @@
 - sourmash
 - SRA Toolkit (disable file caching)
 
+## Metadata
+
+```
+curl ftp://climb.genomics.cn/pub/10.5524/200001_201000/200001/seq_file_mapping_to_SRA.txt | sed 's/\r$//' | perl -ne 's/_(\d)fastq.gz/_$1.fastq.gz/; print' | gzip -c > metadata.tsv.gz
+```
+
 ## Pipeline execution
 
 ```
--- dodo.py
+++ dodo.py
@@ -2,50 +2,16 @@ from __future__ import print_function
 import os
 import pandas
 from doit.tools import run_once
-try:
-    from urllib.request import urlretrieve
-except:
-    from urllib import urltrerieve
-
-
-def task_get_metadata():
-    def get_metadata(targets):
-        url = ('ftp://climb.genomics.cn/pub/10.5524/200001_201000/200001/'
-               'seq_file_mapping_to_SRA.txt')
-        urlretrieve(url, targets[0])
-        return True
-
-    return {
-        'actions': [get_metadata],
-        'targets': ['metadata.tsv'],
-        'uptodate': [run_once],
-    }
-
-
-def task_get_samples():
-    def get_samples(targets):
-        metadata = pandas.read_table('metadata.tsv')
-        with open('sample-accessions.txt', 'w') as outfile:
-            for accession in metadata['Sample_Acc'].unique():
-                print(accession, file=outfile)
-
-    return {
-        'actions': [get_samples],
-        'targets': ['sample-accessions.txt'],
-        'file_dep': ['metadata.tsv'],
-    }
 
 
 def task_download():
-    if not os.path.exists('metadata.tsv'):
-        return None
-
-    metadata = pandas.read_table('metadata.tsv')
+    metadata = pandas.read_table('metadata.tsv.gz')
     for accession in sorted(metadata['Run_acc'].unique()):
         outfile = 'sra-fastq/{prefix}/{acc}.fq.gz'.format(prefix=accession[:5], acc=accession)
         cmd = 'fastq-dump --split-files -Z {acc} | gzip -c > {of}'.format(acc=accession, of=outfile)
         yield {
             'name': outfile,
+            'file_dep': ['metadata.tsv.gz'],
             'actions': [cmd],
             'targets': [outfile],
             'uptodate': [run_once]
